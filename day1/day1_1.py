6ba947ff79d271779da42344a0b2caae3c59d229
Isaac Beale <46497763+ibeale@users.noreply.github.com>
2020-12-01 11:46:37 -0700

    First day completed

---
+++ day1/day1_1.py
@@ -0,0 +1,21 @@
+with open("input.txt", "r") as f:
+    data = f.readlines()
+
+data = list(map(lambda x : int(x), data))
+data.sort()
+TARGET = 2020
+
+left = 0
+right = len(data) - 1
+
+while(data[left] + data[right] != TARGET):
+    if data[left] + data[right] < TARGET:
+        left += 1
+    else:
+        right -= 1
+if(data[left] + data[right] == TARGET):
+    print(left, right)
+    print(data[left] * data[right])
+else:
+    print("No solution found")
+
---
+++ day1/day1_2.py
@@ -0,0 +1,18 @@
+with open("input.txt", "r") as f:
+    data = f.readlines()
+
+data = list(map(lambda x : int(x), data))
+data.sort()
+TARGET = 2020
+
+for idx,curVal in enumerate(data):
+    left = idx
+    right = len(data) - 1
+    while(data[left] + data[right] + curVal != TARGET and left < right):
+        if data[left] + data[right] + curVal < TARGET:
+            left += 1
+        else:
+            right -= 1
+    if(data[left] + data[right] + curVal == TARGET):
+        print(data[left], data[right], curVal)
+        print(data[left] * data[right] * curVal)
